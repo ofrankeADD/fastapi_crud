db89bf00fd500ad5a5accf7ccc09ca8280d50613
ofrankeADD <ofranke@posteo.de>
2022-01-29 18:42:43 +0100

    using pytest fixture

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,36 +1,30 @@
-from starlette.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_ping():
-    response = client.get("/ping")
+def test_ping(test_app):
+    response = test_app.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!"}
 
-def test_items():#(item_id: int, q: Optional[str] = None):
-    response = client.get("/items/42")
+def test_items(test_app):#(item_id: int, q: Optional[str] = None):
+    response = test_app.get("/items/42")
     assert response.status_code == 200
     assert response.json() == {"item_id": 42, "q": None}
     
-    response = client.get("/items/foo")
+    response = test_app.get("/items/foo")
     assert response.status_code == 422
 
-def test_query():
-    response = client.get("/items/423?q=bar123")
+def test_query(test_app):
+    response = test_app.get("/items/423?q=bar123")
     assert response.status_code == 200
     assert response.json() == {"item_id": 423, "q": "bar123"}
     
-def test_postItem():
+def test_postItem(test_app):
     json_item = {"name": "Mais", "price": 2.45}
-    response = client.post("/items/", json=json_item)
+    response = test_app.post("/items/", json=json_item)
     assert response.status_code == 200
     assert response.json() == {"item_name": "Mais", "item_price": 2.45}
     
-def test_putItem():
+def test_putItem(test_app):
     json_item = {"name": "Gurke", "price": 1.33, "is_offer": True}
-    response = client.put("/items/112", json=json_item)
+    response = test_app.put("/items/112", json=json_item)
     assert response.status_code == 200
     assert response.json() == {"item_name": "Gurke", "item_id": 112}
     
