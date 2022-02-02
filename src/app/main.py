da2e313451f88081ab5d9b31eebd0e6472b13af1
ofrankeADD <ofranke@posteo.de>
2022-02-02 15:15:50 +0100

    initial postgres integration commit

--- src/app/main.py
+++ src/app/main.py
@@ -16,7 +16,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World HTTPS on the fly high!"}
+    return {"Hello": "World HTTPS on the fly high with postgres!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
