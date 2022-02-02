7ad7070cea472ba9c8de71aea10e2baa2778f190
ofrankeADD <ofranke@posteo.de>
2022-02-02 18:00:09 +0100

    implement crud POST for notes model

--- src/app/db.py
+++ src/app/db.py
@@ -1,12 +1,20 @@
 import os
+import sqlalchemy as sqla
 from databases import Database
-from sqlalchemy import create_engine, MetaData
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
+engine = sqla.create_engine(DATABASE_URL)
+metadata = sqla.MetaData()
+notes = sqla.Table(
+    "notes",
+    metadata,
+    sqla.Column("id", sqla.Integer, primary_key=True),
+    sqla.Column("title", sqla.String(50)),
+    sqla.Column("description", sqla.String(50)),
+    sqla.Column("created_date", sqla.DateTime, default=sqla.sql.func.now(), nullable=False),
+)
 
 # databases query builder
 database = Database(DATABASE_URL)
--- src/app/main.py
+++ src/app/main.py
@@ -1,18 +1,35 @@
 from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
-from app.api import ping
+
+from app.api import notes, ping
+from app.db import engine, database, metadata
+
+
+metadata.create_all(engine)
 
 app = FastAPI()
 
-app.include_router(ping.router)
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+app.include_router(ping.router)
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
+
+
+from pydantic import BaseModel
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
-
 
 @app.get("/")
 def read_root():
