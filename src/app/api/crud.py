6fa6a2c887320e4150570086b15332ba181fe302
ofrankeADD <ofranke@posteo.de>
2022-02-04 01:35:21 +0100

    implemented GET, PUT, DELETE routes

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -5,3 +5,24 @@ from app.db import notes, database
 async def post(note_payload: NoteSchema):
     query = notes.insert().values(title=note_payload.title, description=note_payload.description)
     return await database.execute(query=query)
+
+
+async def get_one(id: int):
+    query = notes.select().where(id == notes.c.id)
+    return await database.fetch_one(query=query)
+
+
+async def get_all():
+    query = notes.select()
+    return await database.fetch_all(query=query)
+
+
+async def put(id: int, note_payload: NoteSchema):
+    query = notes.update().where(id == notes.c.id).values(title=note_payload.title, description=note_payload.description).returning(notes.c.id)
+    return await database.execute(query=query)
+
+
+async def delete(id: int):
+    query = notes.delete().where(id == notes.c.id)
+    return await database.execute(query=query)
+
--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from typing import List
 
 from app.api import crud
 from app.api.models import NoteDB, NoteSchema
@@ -13,8 +14,68 @@ async def create_note(note_payload: NoteSchema):
     response_object = {
         "id": note_id,
         "title": note_payload.title,
-        "description": note_payload.description,
+        "description": note_payload.description
     }
     alternative_response_object = {**note_payload.dict(), "id": note_id}
     
     return response_object
+
+
+@router.get("/{id}/", response_model=NoteDB)
+async def read_one_note(id: int):
+    note = await crud.get_one(id)
+    
+    if not note:
+        raise HTTPException(status_code=404, detail=f"Note with id {id} not found!")
+    
+    response = {
+        "id": id,
+        "title": note.title,
+        "description": note.description
+    }
+    #response = {**note.dict(), "id": note_id}
+    return response
+
+
+@router.get("/", response_model=List[NoteDB])
+async def read_all_notes():
+    return await crud.get_all()
+    
+    #if not note_payload:
+    #    raise HTTPException(status_code=404, detail="There are no notes!")
+    #
+    #response = []
+    #for note in note_payload:
+    #    response.append({**note_payload.dict()})
+    #return response
+    
+
+@router.put("/{id}/", response_model=NoteDB)
+async def update_note(id: int, note_payload: NoteSchema):
+    note = await crud.get_one(id)
+    
+    if not note:
+        raise HTTPException(status_code=404, detail=f"Note with id {id} not found!")
+    
+    note_id = await crud.put(id, note_payload)
+    #response = {
+    #    "id": note_id,
+    #    "title": note_payload.title,
+    #    "description": note_payload.description
+    #}
+    response = {**note_payload.dict(), "id": note_id}
+ 
+    return response
+
+
+@router.delete("/{id}/", response_model=NoteDB)
+async def delete_note(id: int):
+    note = await crud.get_one(id)
+    
+    if not note:
+        raise HTTPException(status_code=404, detail=f"Note with id {id} not found!")
+    
+    return await crud.delete(id)
+
+
+
