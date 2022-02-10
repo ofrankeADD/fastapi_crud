d22586c419982a7c9fb3473f8c7e4fb4669d9f3a
ofrankeADD <ofranke@posteo.de>
2022-02-11 00:55:41 +0100

    moved web to src/backend/ && added frontend service to yaml

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8432'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
